05f4d8b5b53cc84e7a7b64d9d19ed7340270e0b6
Glen Alloy <glenalloy8@gmail.com>
2024-07-19 13:03:21 +0530

    Student DTO completed in deserialization

--- Deserialization/src/main/java/org/example/controller/Student.java
+++ Deserialization/src/main/java/org/example/controller/Student.java
@@ -1,4 +1,7 @@
 package org.example.controller;
 
 public class Student {
+    public static void main(String[] args) {
+
+    }
 }
--- Deserialization/src/main/java/org/example/dto/StudentDto.java
+++ Deserialization/src/main/java/org/example/dto/StudentDto.java
@@ -1,4 +1,16 @@
 package org.example.dto;
 
-public class StudentDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class StudentDto implements Serializable {
+    private String name;
+    private String age;
+    private String address;
 }
